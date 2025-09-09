0f414a19362266a936bd8f3233bd686be87a78f5
Maanyak2006 <maanya62006@gmail.com>
2025-09-09 21:03:26 +0530

    Create IncDecExample.java

---
+++ IncDecExample.java
@@ -0,0 +1,11 @@
+public class IncDecExample{
+    public static void main(String[] args){
+        int x=10;
+        System.out.println("original:"+x);
+        System.out.println("post increment:"+(x++));
+        System.out.println("after post increment:"+x);
+        System.out.println("pre increment:"+(++x));
+        System.out.println("post decrement:"+(x--));
+        System.out.println("pre decrement:"+(--x));
+    }
+}
