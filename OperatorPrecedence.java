987eff8a03ead8ce3d72b4b62e72d6451203e93b
Maanyak2006 <maanya62006@gmail.com>
2025-09-16 20:12:03 +0530

    Create OperatorPrecedence.java

---
+++ OperatorPrecedence.java
@@ -0,0 +1,7 @@
+public class OperatorPrecedence{
+public static void main(String[] args){
+int result= 10+5*2;
+//result= 10 +(5 * 2)=10+10=20
+System.out.println("Result without parentheses: " + result);
+}
+}
