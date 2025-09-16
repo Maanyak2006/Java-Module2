e9351ea5d1030306c5a0c773d88ffc5a70dac5cd
Maanyak2006 <maanya62006@gmail.com>
2025-09-16 20:26:32 +0530

    Create If_Else_Example.java

---
+++ If_Else_Example.java
@@ -0,0 +1,10 @@
+public class If_Else_Example{
+public static void main(String[] args){
+int number=15;
+if (number% 2==0){
+System.out. println(number + " is Even”);
+}else{
+System.out.println(number + " is Odd");
+}
+}
+}
